4a7ff47fd6d3834ace4d37193980613ce83c5085
bpopadiuk <pboris@pdx.edu>
2019-06-04 10:20:42 -0700

    clean up

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,6 @@ struct BadModel {
 }
 
 fn main() {
-    // Here's a demonstration of what the MigrateTable macro is doing for us
-    let (name, fields) = Model::generate_schema();
-    println!("NAME: {:?}\nFIELDS: {:?}", name, fields);
-
     // Usually we'll just be calling it as an argument to the create_table() method though
     let mut db = lib::DB::new("some_dsn_here");
 
@@ -33,6 +29,12 @@ fn main() {
         name: "Boris".to_string(),
         age: 27,
         birthday: "someday".to_string(),
+    };
+
+    let j = Model {
+        name: "Jordan".to_string(),
+        age: 8,
+        birthday: "idk".to_string(),
     };
 
     // Example of create_table returning an error when passed a model struct containing an illegal type
@@ -45,12 +47,20 @@ fn main() {
     db.insert("Model", sql!(name = "Jordan", age = 8, birthday = "idk"))
         .unwrap();
 
+    db.insert(
+        "Model",
+        sql!(name = "Boris", age = 27, birthday = "whatever"),
+    )
+    .unwrap();
+
     //Deleting items from the database looks just like inserting
     //All records that match the provided conditions will be deleted
     //SQLITE does not throw an error if no records match the conditions provided
     //Errors that arise will result from column names that are mispleed or don't exist
     //can allow specificy one condtion per table column
-    db.delete("Model", sql!(name = "Jordan", age = 10)).unwrap();
+    db.delete("Model", sql!(name = "Someotherguy", age = 10))
+        .unwrap();
+    db.delete("Model", sql!(name = "Boris", age = 27)).unwrap();
 
     // This one should fail...
     let result2 = db.insert(
@@ -61,5 +71,8 @@ fn main() {
 
     let mut out: Vec<Model> = Vec::new();
     db.select_all("Model", &mut out).unwrap();
-    println!("IN:  {:?}\nOUT: {:?}", inp, out);
+    println!(
+        "IN:      {:?}\t{:?}\nDELETED: {:?}\nOUT: {:?}",
+        inp, j, inp, out
+    );
 }
