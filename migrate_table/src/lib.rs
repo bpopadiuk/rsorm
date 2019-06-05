35441428ff6e822c8cfbce406986a86398d2adca
jordan-childs20 <jsd9@pdx.edu>
2019-06-04 20:58:56 -0700

    added rust-doc for generate_schema()

--- migrate_table/src/lib.rs
+++ migrate_table/src/lib.rs
@@ -1,3 +1,6 @@
 pub trait MigrateTable {
+    /// Generates a database schema in the form of a tuple. <br>
+    /// Name is the name of the struct/table.<br>
+    /// Called like: `**struct_name**::generate_schema()`
     fn generate_schema() -> (String, Vec<(String, String)>);
 }
